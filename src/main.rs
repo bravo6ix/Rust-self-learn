2bf991758b2809821ba07ec3c70a5094da07dec6
Ming <qq15815732077@gmail.com>
2024-09-05 03:55:47 +0800

    PUSH

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    greet_world();
+}
+
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界!";
+    let spanish = "Hola Mundo";
+    let english = "Hello World";
+    let regions = [southern_germany, chinese, spanish, english];
+    for region in regions.iter() {
+        println!("{}", region);
+    }
 }
